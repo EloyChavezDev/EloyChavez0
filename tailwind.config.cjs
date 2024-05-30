486f7c6e14bb8fda8e3d50e8ca865b98085fba7f
EloyChavezDev <laneloychavez@gmail.com>
2024-05-30 13:57:09 -0600

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,17 +9,8 @@ module.exports = {
 		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: ["light", "dark", "cupcake"], // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
+		fontFamily: {
+			'sans': ['"Press Start 2P"', ...defaultTheme.fontFamily.sans],
+		  },
 	  }
-}
-
-const defaultTheme = require('tailwindcss/defaultTheme')
-
-module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        'sans': ['"Press Start 2P"', ...defaultTheme.fontFamily.sans],
-      },
-    }
-  }
 }
