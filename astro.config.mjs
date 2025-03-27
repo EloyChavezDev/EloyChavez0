72c0c6431cd58419f5b9bad2c7497fba09e5eb77
EloyChavezDev <laneloychavez@gmail.com>
2025-03-26 20:03:25 -0600

    change url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import { join } from 'path';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://eloychavez.dev',
   integrations: [mdx(), sitemap(), tailwind(), image(
     {
       serviceEntryPoint: '@astrojs/image/sharp',
